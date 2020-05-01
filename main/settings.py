d36e1a69218af1c6f8d3ae875a63697c9edff1d7
cdaza <cdaza933@hotmail.com>
2020-05-01 17:30:37 -0500

    commit

--- main/settings.py
+++ main/settings.py
@@ -119,11 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = BASE_DIR + '/admin/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'main.backend.MyPortalBackend',
 ]
-
-STATIC_ROOT = BASE_DIR + "/"
