85328fa42c4300040c6e8c5541a2f872e5523cf7
cdaza <cdaza933@hotmail.com>
2020-05-16 13:41:06 -0500

    test

--- companies/tests.py
+++ companies/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+import json
+import os
+from random import randint
 
-# Create your tests here.
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+django.setup()
+from django.test import TestCase
+from rest_framework.test import APIClient
+from companies.models import Company
+
+
+
+class AddOpenQuestionTestCase(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.url = '/api/company/register'
+        self.headers = {'Content-Type': 'application/json'}
+
+    def test_add_company(self):
+        nit = "3"
+        if Company.objects.filter(nit=nit).exists():
+            nit += str(randint(0, 10))
+        payload = {
+            "nit": nit,
+            "name": "3",
+            "address": "3",
+            "password": "pass",
+            "confirm_password": "pass"
+        }
+        response = self.client.post(self.url,  payload, format='json')
+        current_data = json.loads(response.content)
+        company = Company.objects.filter(nit=nit)
+        self.assertTrue(company.exists())
+        company = company.first()
+        self.assertEquals(company.name, payload['name'])
+        self.assertEquals(company.address, payload['address'])
--- main/settings.py
+++ main/settings.py
@@ -81,8 +81,15 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {'default': dj_database_url.config()}
+# DATABASES = {'default': dj_database_url.config()}
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -132,3 +139,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+REST_FRAMEWORK = {
+    'PAGE_SIZE': 10,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+}
